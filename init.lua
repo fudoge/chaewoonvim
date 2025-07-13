9002ff481653f2bb178daacb7418570036af995d
fudoge <kchawono@naver.com>
2025-07-14 08:51:50 +0900

    Added Opacity

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
-vim.g.mkdp_auto_start = 1
-vim.g.mkdp_auto_close = 1
+vim.g.mkdp_auto_start = 0
+vim.g.mkdp_auto_close = 0
 vim.g.mkdp_refresh_slow = 0
 vim.g.mkdp_command_for_global = 0
 vim.g.mkdp_open_to_the_world = 0
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -99,3 +99,11 @@ vim.diagnostic.config({
         },
     },
 })
+
+-- opacity
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
